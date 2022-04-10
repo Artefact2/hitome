7569977a5a5ec1917c08957a64f492cc4b766b38
Romain “Artefact2” Dal Maso <romain.dalmaso@artefact2.com>
2022-04-10 19:27:49 +0200

    Add PSI

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct Settings {
     refresh: u64,
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
+#[derive(PartialEq, PartialOrd, Clone, Copy)]
 struct Bytes(u64);
 
 impl fmt::Display for Bytes {
@@ -23,19 +23,29 @@ impl fmt::Display for Bytes {
         if self.0 >= 10000 * 1024 * 1024 * 1024 {
             write!(
                 f,
-                "{:7.2}T",
+                "{:>7.2}T",
                 self.0 as f32 / (1024. * 1024. * 1024. * 1024.)
             )
         } else if self.0 >= 10000 * 1024 * 1024 {
-            write!(f, "{:7.2}G", self.0 as f32 / (1024. * 1024. * 1024.))
+            write!(f, "{:>7.2}G", self.0 as f32 / (1024. * 1024. * 1024.))
         } else if self.0 >= 10000 * 1024 {
-            write!(f, "{:7.2}M", self.0 as f32 / (1024. * 1024.))
+            write!(f, "{:>7.2}M", self.0 as f32 / (1024. * 1024.))
         } else {
-            write!(f, "{:7.2}K", self.0 as f32 / 1024.)
+            write!(f, "{:>7.2}K", self.0 as f32 / 1024.)
         }
     }
 }
 
+#[derive(PartialEq, PartialOrd, Clone, Copy)]
+struct Percentage(f32);
+
+impl fmt::Display for Percentage {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:>7.2}%", self.0)
+    }
+}
+
+#[derive(Clone, Copy)]
 struct Threshold<T> {
     val: T,
     med: T,
@@ -46,18 +56,18 @@ struct Threshold<T> {
 
 impl<T> fmt::Display for Threshold<T>
 where
-    T: fmt::Display + Ord,
+    T: fmt::Display + PartialOrd,
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if !self.smart || self.val.cmp(&self.med) == Ordering::Less {
+        if !self.smart || self.val.partial_cmp(&self.med) == Some(Ordering::Less) {
             /* < med or dumb terminal */
             return write!(f, "{}", self.val);
         }
 
-        if self.val.cmp(&self.high) == Ordering::Less {
+        if self.val.partial_cmp(&self.high) == Some(Ordering::Less) {
             /* < high: we're med */
             write!(f, "\x1B[1;93m{}\x1B[0m", self.val)
-        } else if self.val.cmp(&self.crit) == Ordering::Less {
+        } else if self.val.partial_cmp(&self.crit) == Some(Ordering::Less) {
             /* < crit: we're high */
             write!(f, "\x1B[1;91m{}\x1B[0m", self.val)
         } else {
@@ -199,6 +209,98 @@ impl<'a> fmt::Display for MemoryStats<'a> {
     }
 }
 
+struct PressureStats<'a> {
+    settings: &'a Settings,
+}
+
+impl<'a> PressureStats<'a> {
+    fn new(s: &'a Settings) -> PressureStats {
+        PressureStats { settings: s }
+    }
+}
+
+impl<'a> fmt::Display for PressureStats<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut cells = [Threshold {
+            val: Percentage(0.0),
+            med: Percentage(1.0),
+            high: Percentage(5.0),
+            crit: Percentage(10.0),
+            smart: self.settings.smart,
+        }; 18];
+
+        [
+            /* XXX: pass through mutable slices? */
+            ("cpu", 0, 3),
+            ("memory", 6, 9),
+            ("io", 12, 15),
+        ]
+        .map(|s| {
+            let mut p = std::path::PathBuf::from("/proc/pressure");
+            p.push(s.0);
+            let pressure = match std::fs::read_to_string(p) {
+                Ok(a) => a,
+                _ => return,
+            };
+            for line in pressure.lines() {
+                let mut elems = line.split_ascii_whitespace();
+                let idx: usize;
+
+                match elems.nth(0) {
+                    Some("some") => idx = s.1,
+                    Some("full") => idx = s.2,
+                    _ => continue,
+                }
+
+                for el in elems {
+                    let (idx, p) = match el.rsplit_once('=') {
+                        Some(("avg10", p)) => (idx, p),
+                        Some(("avg60", p)) => (idx + 1, p),
+                        Some(("avg300", p)) => (idx + 2, p),
+                        _ => continue,
+                    };
+                    cells[idx].val.0 = p.parse::<f32>().unwrap();
+                }
+            }
+        });
+
+        let w = self.settings.colwidth;
+        let newline = newline(self.settings.smart);
+        let (hdrbegin, hdrend) = headings(self.settings.smart);
+        write!(
+            f,
+            "{}{:>w$} {:>w$} {:>w$} {:>w$} {:>w$} {:>w$} {:>w$}{}{}",
+            hdrbegin,
+            "PSI",
+            "SOME_CPU",
+            "FULL_CPU",
+            "SOME_MEM",
+            "FULL_MEM",
+            "SOME_IO",
+            "FULL_IO",
+            hdrend,
+            newline
+        )?;
+
+        for el in [("avg10", 0), ("avg60", 1), ("avg300", 2)] {
+            write!(
+                f,
+                "{:>w$} {:>w$} {:>w$} {:>w$} {:>w$} {:>w$} {:>w$}{}",
+                el.0,
+                cells[el.1],
+                cells[el.1 + 3],
+                cells[el.1 + 6],
+                cells[el.1 + 9],
+                cells[el.1 + 12],
+                cells[el.1 + 15],
+                newline
+            )?;
+        }
+
+        write!(f, "{}", newline)
+    }
+}
+
 fn newline(smart: bool) -> &'static str {
     if smart {
         "\x1B[0K\n"
@@ -239,6 +341,7 @@ fn main() {
 
     let mut w = BufWriter::new(io::stdout());
     let mem = MemoryStats::new(&settings);
+    let psi = PressureStats::new(&settings);
 
     loop {
         let t = Instant::now();
@@ -250,7 +353,7 @@ fn main() {
             writeln!(w, "----------").unwrap();
         }
 
-        write!(w, "{}", mem).unwrap();
+        write!(w, "{}{}", mem, psi).unwrap();
 
         if settings.smart {
             /* Erase from cursor to end */
